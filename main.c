1ade28cf8b6044fd16e57c42381c3b3d2d478fd5
Gwyndaf Evans <gwyndaf.evans@diamond.ac.uk>
2003-06-28 00:37:45 +0000

    Dump intermediate data for pooch plotting; silent switch used; new PSplot routine called

--- main.c
+++ main.c
@@ -33,8 +33,10 @@
 #include "chooch.h"
 int c;
 char  *sElement="Se";           // Letter symbol for element name e.g. Au, Se, I
+char cScanTitle[TITLE];
 int id1=0, id2=0;
-int verbose;
+int verbose, silent;
+double fpInfl, fppInfl, fpPeak, fppPeak, EInfl, EPeak;
 double fE1=0.0, fE2=0.0, fE3=0.0, fE4=0.0;
 double fEres=0.00014;
 //typedef struct{double d1; double d2; double d3;} deriv; 
@@ -50,9 +52,10 @@ int main(int argc, char *argv[])
   char  *sEdge="K";           // Letter symbol for absorption edge K, L1, L2, L3, M
   char opt;
   //
-  int nDataPoints, nFit, nPoints, plotX=0, psplot=0;
+  int nDataPoints, nFit, nPoints, plotX=0, psplot=0, display=0;
   double dE, tmp, fEdge;
   double fXraw[MAXSIZE], fYraw[MAXSIZE];
+  double fYfita[MAXSIZE], fYfitb[MAXSIZE];
   double fYspline[MAXSIZE], fXfpp[MAXSIZE];
   double fYsmooth[MAXSIZE], fYnorm[MAXSIZE];
   double fYfpp[MAXSIZE], fYfpps[MAXSIZE], fYfp[MAXSIZE];
@@ -61,59 +64,67 @@ int main(int argc, char *argv[])
   //
   double SeK = 12665.0;
   double fC, fM;
+  FILE *ff;
   //
-  verbose = 0;
+  verbose=silent=0;
   optarg = NULL;
-  while( ( opt = getopt( argc, argv, "he:a:r:xo:p:v:1:2:3:4:" ) ) != (char)(-1) )
+  while((opt = getopt(argc, argv, "she:a:r:xo:p:v:1:2:3:4:d")) != (char)(-1))
      switch( opt ) {
+     case 's' :
+	silent = 1;
+	break;
      case 'h' :
 	(void)usage();
 	exit(EXIT_SUCCESS);
 	break;
      case 'e' :
 	sElement = optarg;
-	printf("-e     Atomic element = %s\n", sElement);
+	if(!silent)printf("-e     Atomic element = %s\n", sElement);
 	break;
      case 'a' :
 	sEdge = optarg;
-	printf("-a:   Absorption edge = %s\n", sEdge);
+	if(!silent)printf("-a:   Absorption edge = %s\n", sEdge);
 	break;
      case 'r' :
 	fEres = atof(optarg);
-	printf("-r: Energy resolution = %f\n", fEres);
+	if(!silent)printf("-r: Energy resolution = %f\n", fEres);
 	break;	
      case 'x' :	
 	plotX = 1;
-	printf("-x: with X plotting \n");
+	if(!silent)printf("-x: with X plotting \n");
 	break;
      case 'o' :	
 	outfile = optarg;
-	printf("-o:  Output file name = %s\n", outfile);
+	if(!silent)printf("-o: Output file name = %s\n", outfile);
 	break;
      case 'p' :	
 	psplot = 1;
 	psfile = optarg;
-	printf("-p:    PS output file = %s\n", psfile);
+	if(!silent)printf("-p: PS output file = %s\n", psfile);
 	break;
      case 'v' :
 	verbose = atoi(optarg);
-	printf("Verbosity level %d", verbose);
+	if(!silent)printf("-v: Verbosity level %d", verbose);
 	break;
      case '1' :
 	fE1 = atof(optarg);
-	printf("Below edge fit lower limit = %f\n", fE1);
+	if(!silent)printf("-1: Below edge fit lower limit = %f\n", fE1);
 	break;
      case '2' :
 	fE2 = atof(optarg);
-	printf("Below edge fit upper limit = %f\n", fE2);
+	if(!silent)printf("-2: Below edge fit upper limit = %f\n", fE2);
 	break;
      case '3' :
 	fE3 = atof(optarg);
-	printf("Above edge fit lower limit = %f\n", fE3);
+	if(!silent)printf("-3: Above edge fit lower limit = %f\n", fE3);
 	break;
      case '4' :
 	fE4 = atof(optarg);
-	printf("Above edge fit upper limit = %f\n", fE4);
+	if(!silent)printf("-4: Above edge fit upper limit = %f\n", fE4);
+	break;
+     case 'd' :
+	display = 1;
+	if(!silent)printf("-d: Dump data file for pooch\n", fE4);
 	break;
      }  
   if(argc < 2){
@@ -122,7 +133,7 @@ int main(int argc, char *argv[])
     exit(EXIT_FAILURE);
   }
   sFilename = argv[optind];
-  printf("Fluorescence scan filename: %s\n", sFilename);
+  if(!silent)printf("Fluorescence scan filename: %s\n", sFilename);
   //
   if(plotX){
      id1=cpgopen("/xw");
@@ -137,7 +148,7 @@ int main(int argc, char *argv[])
   err=checks(nDataPoints, fXraw, fYraw, &dE);
   fMid=(fXraw[nDataPoints-1]+fXraw[0])/2.0;
   sEdge=get_Edge(sElement, fMid, &fEdge);
-  printf("\nSpectrum over %s %s edge at theoretical energy of %8.2f eV\n", sElement, sEdge, fEdge);
+  if(!silent)printf("\nSpectrum over %s %s edge at theoretical energy of %8.2f eV\n", sElement, sEdge, fEdge);
   /*
    * Smooth with Savitzky-Golay filter
    */ 
@@ -149,7 +160,7 @@ int main(int argc, char *argv[])
   /*
    * Normalise data
    */
-  err=normalize(nDataPoints, fEdge, fXraw, fYraw, fYnorm, plotX);
+  err=normalize(nDataPoints, fEdge, fXraw, fYraw, fYnorm, plotX, fYfita, fYfitb);
   if(plotX)spacebar();
   /*
    * Impose on theoretical spectrum of f'' to obtain 
@@ -182,6 +193,8 @@ int main(int argc, char *argv[])
    * Perform Kramer-Kronig transform
    */
   Integrate(nDataPoints, &nPoints, fEdge, fXraw, fXfpp, fYspline, fYfpps, fYDeriv1, fYDeriv2, fYDeriv3, fYfp);
+  err=selwavel(nDataPoints, fXfpp, fYspline, fYfp);
+
   if(plotX)
      addline(nPoints, fXfpp, fYspline, BLUE);
   /*
@@ -193,6 +206,20 @@ int main(int argc, char *argv[])
      spacebar();
   }
   if(psplot){
-     efsplot(nPoints, fXfpp, fYspline, fYfp, 1, psfile);
+     /*     efsplot(nPoints, fXfpp, fYspline, fYfp, 1, psfile);*/
+     psplt(nPoints, fXfpp, fYspline, fYfp, psfile);
+  }
+  /*
+   *
+   */
+  if(display){
+     if ((ff = fopen(".pooch", "w")) == NULL) {
+	printf("Cannot open .pooch for write\n");
+	exit(EXIT_FAILURE);
+     }
+     for (i = 0; i < nDataPoints; i++) {
+	fprintf(ff,"%10.3f %7.2f %7.2f %7.2f %7.3f %7.3f %7.3f %7.3f\n", fXraw[i], fYraw[i], fYsmooth[i], fYfita[i], fYfitb[i], fYDeriv1[i], fYDeriv2[i], fYDeriv3[i]);
+     }
+     fclose(ff);
   }
 }
