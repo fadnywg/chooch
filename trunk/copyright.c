f2286303e820f906cf8d363a8f79d32c18b81b0a
gwyndaf <gwyndaf@abc6948b-5dc3-4742-95c1-463526e1a57d>
2003-08-13 21:23:36 +0000

    Errors in release 1.0 corrected - now 1.0.1!

--- trunk/copyright.c
+++ trunk/copyright.c
@@ -21,7 +21,8 @@
 void copyright()
 {
   (void)fprintf( stderr, "==================================================\n" );
-  (void)fprintf( stderr, " CHOOCH by Gwyndaf Evans Copyright (C) 1994--2003 \n" );
+  (void)fprintf( stderr, "                  chooch 1.0.1                    \n");
+  (void)fprintf( stderr, "      by Gwyndaf Evans Copyright (C) 1994--2003   \n" );
   (void)fprintf( stderr, "             gwyndaf@gwyndafevans.co.uk\n\n" );
   (void)fprintf( stderr, "        G. Evans & R. F. Pettifer (2001)\n");
   (void)fprintf( stderr, "           J. Appl. Cryst. 34, 82-86.\n");
