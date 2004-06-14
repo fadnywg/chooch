82213e8c7b36275fe1bee6bf794ba7dd387e5194
nobody <nobody@abc6948b-5dc3-4742-95c1-463526e1a57d>
2004-06-14 12:02:17 +0000

    This commit was manufactured by cvs2svn to create tag 'release-5-0'.

---
+++ tags/release-5-0/printbanner.c
@@ -0,0 +1,32 @@
+/***************************************************************************
+                          printbanner.c  -  just like it sounds
+                             -------------------
+    begin                : Sun Mar  10 16:14:02 GMT 2002
+    copyright            : (C) 2002 by Gwyndaf Evans
+    email                : gwyndaf@gwyndafevans.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void printbanner()
+{
+  printf("==========================\n");
+  printf("chooch                  \n");
+  printf("Gwyndaf Evans              \n");
+  printf("gwyndaf@gwyndafevans.co.uk       \n");
+  printf("==========================\n");
+}
