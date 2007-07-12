439d0d656628be44d825671e44727354a21bebf3
gwyndaf <gwyndaf@abc6948b-5dc3-4742-95c1-463526e1a57d>
2007-07-12 18:56:17 +0000

    added savwin

---
+++ trunk/savwin.c
@@ -0,0 +1,41 @@
+/***************************************************************************
+                    license.c  -  shows license information
+                             -------------------
+    begin                : Mon Dec  1 20:26:00 CET 2003
+    copyright            : (C) 2002 by Gwyndaf Evans
+    email                : gwyndaf@gwyndafevans.co.uk
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "chooch.h"
+
+void savwin(double fEres, double fEdge, double dE, int *nSavWin) {
+  
+  double fMonoRes;
+  extern int verbose; 
+  
+  fMonoRes = fEres * fEdge;
+  *nSavWin = (int) (fMonoRes / dE);
+  if(*nSavWin > 29){ 
+    *nSavWin=29;
+  }
+  if(*nSavWin < 2){ 
+     *nSavWin=2;
+  }
+  if(verbose>0)printf("dE = %f Resol = %f\n", dE, fMonoRes);
+  if(verbose>0)printf("Savitsky-Golay window value = %d\n", nSavWin);
+}
