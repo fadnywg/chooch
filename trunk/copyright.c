e87c6cb672f37deb7144481770cabfdf26408a81
gwyndaf <gwyndaf@abc6948b-5dc3-4742-95c1-463526e1a57d>
2008-03-18 18:33:35 +0000

    Updated year to 2008

--- trunk/copyright.c
+++ trunk/copyright.c
@@ -2,7 +2,7 @@
                              copyright.c  -  prints copyright
                              -------------------
     begin                : Sat July  6 12:01:02 GMT 2002
-    copyright            : (C) 1994--2003 by Gwyndaf Evans
+    copyright            : (C) 1994--2008 by Gwyndaf Evans
     email                : gwyndaf@gwyndafevans.co.uk
  ***************************************************************************/
 
@@ -20,7 +20,7 @@ void copyright()
 {
   (void)fprintf( stderr, "==================================================\n" );
   (void)fprintf( stderr, "                     chooch-5.0.6                 \n");
-  (void)fprintf( stderr, "      by Gwyndaf Evans Copyright (C) 1994--2007   \n" );
+  (void)fprintf( stderr, "      by Gwyndaf Evans Copyright (C) 1994--2008   \n" );
   (void)fprintf( stderr, "             gwyndaf@gwyndafevans.co.uk\n\n" );
   (void)fprintf( stderr, "        G. Evans & R. F. Pettifer (2001)\n");
   (void)fprintf( stderr, "           J. Appl. Cryst. 34, 82-86.\n");
